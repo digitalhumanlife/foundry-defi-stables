c0093efdff36676492e896f82dc8c41f48a6bb31
JW <digitalhumanlife@gmail.com>
2023-08-16 20:08:49 +0900

    initial

---
+++ test/DecentralizedStableCoin.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DeployDecentralizedStableCoin} from "../script/DeployDecentralizedStableCoin.sol";
+
+contract DecentralizedStableCoinTest {}
